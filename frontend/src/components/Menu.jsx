8284863db278fff4bc4f197431417019b2105c96
Karl-Johannes Ränkel <karljr@taltech.ee>
2025-10-18 18:15:38 +0300

    Tegin algse avalehe ilma funktsionaalsuseta.

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,9 @@
+function Menu() {
+    return (
+        <div style={{ height:'100%' ,marginRight: '3%', marginTop: '4%', padding: '1%' }} >
+            <img src="../../public/DropMenu.png" alt="DropMenu" style={{ height: '100%' }} />
+        </div>
+    );
+}
+
+export default Menu;
