6e845ff58dd76e92bbd0ae19b4d3c167e3450caa
Christofer Toom <chrito@taltech.ee>
2025-10-19 21:31:33 +0300

    Lõin navigatsiooniriba.

---
+++ frontend/src/components/MenuButtonWhite.jsx
@@ -0,0 +1,18 @@
+function MenuButtonWhite() {
+  return (
+    <div
+      style={{
+        height: "100%",
+        backgroundColor: "black",
+      }}
+    >
+      <img
+        src="/DropMenu-White.png"
+        alt="DropMenuWhite"
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+}
+
+export default MenuButtonWhite;
---
+++ frontend/src/components/NavigationBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./NavigationBar.css";
+import Logo from "./Logo";
+import MenuButtonWhite from "./MenuButtonWhite.jsx";
+
+const NavigationBar = () => {
+  return (
+    <nav className="navbar">
+      <a href="/" className="logo">
+        <Logo />
+      </a>
+      <div className="navbar-center">
+        <span className="navbar-text">SaagiSalong</span>
+      </div>
+      <div className="navbar-right">
+        <button className="menubutton">
+          <MenuButtonWhite />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
