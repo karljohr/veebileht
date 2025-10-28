38fa6c561af98b5b2605dab5219e9fed9aed201b
Karl-Johannes Ränkel <karljr@taltech.ee>
2025-10-28 23:27:36 +0200

    Täiendasin profiililehte.

--- frontend/src/components/ProfileButton.jsx
+++ frontend/src/components/ProfileButton.jsx
@@ -1,7 +1,24 @@
-function ProfileButton({width, height, text, color = 'black', fontSize='100%', shadow = "10px 10px 10px lightgray", bcolor = "white"}) {
+import {useState} from "react";
+
+function ProfileButton({
+                           width,
+                           height,
+                           text,
+                           color = 'black',
+                           fontSize='100%',
+                           shadow = "5px 5px 10px lightgray",
+                           bcolor = "rgb(255, 255, 255)"})
+{
+    const [hover, setHover] = useState(false);
+    var hbcolor
+    if (bcolor === "rgb(255, 255, 255)") { hbcolor = "rgb(225, 225, 225)" }
+    else { hbcolor = "rgb(40, 40, 40)" }
+
     return (
         <button
             className="boxes button"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
             style={{
                 border:"2px solid rgba(0, 0, 0, 0.25)",
                 boxShadow:shadow,
@@ -12,7 +29,7 @@ function ProfileButton({width, height, text, color = 'black', fontSize='100%', s
                 alignItems:"center",
                 justifyContent:"center",
                 margin:"0",
-                backgroundColor: bcolor,
+                backgroundColor: hover ? hbcolor : bcolor,
                 cursor: "pointer",
             }}
         >
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -11,21 +11,38 @@ function Profile() {
       <div className="boxes" id="profile">
         <div id="profile_box_title">
           <h1>KASUTAJA ANDMED</h1>
-          <hr id="line" />
         </div>
-        <div id="textbox">
-          <p>
-            Nimi: {FirstName} {LastName}
-          </p>
-          <p>E-mail: {email}</p>
-        </div>
-        <div id="changebox">
-          <ProfileButton
-              text="Muuda"
-              width="100px"
-              height="40px"
-              fontSize="75%"
-          />
+        <div id="profile_box_content">
+            {/*Suure kasti vasak pool*/}
+            <div className="profile_box_content_box">
+                <div id="k">
+                {/*Profiilipilt*/}
+                    <div id="pfp">
+                        <img src="/gambler.png" alt="profile_picture" id="pfp_img" />
+                        <div id="pfp_gray_box"/>
+                    </div>
+                    <div id="textbox">
+                      <p>
+                          <b>{FirstName} {LastName}</b>
+                      </p>
+                      <p>{email}</p>
+                    <div id="changebox">
+                      <ProfileButton
+                          text="Muuda"
+                          width="75px"
+                          height="25px"
+                          fontSize="75%"
+                      />
+                    </div>
+                    </div>
+                </div>
+            </div>
+            {/*Suure kasti parem pool*/}
+            <div className="profile_box_content_box">
+                <div id="opened_box">
+                    <p>WIP</p>
+                </div>
+            </div>
         </div>
       </div>
       <div id="small_box_container">
