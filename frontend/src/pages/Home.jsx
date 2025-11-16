ef0e6acbc55076014f7a860d7ba0ff2a3c1175cc
Christofer Toom <chrito@taltech.ee>
2025-11-16 17:27:03 +0200

    Parandasin avalehe responsiivsust.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,70 +6,38 @@ import "../style/Home.css";
 function Home({ setNavbarOpen }) {
   return (
     <div id="home">
-      <div style={{ backgroundColor: "black", flexGrow: 1 }}>
-        <div
-          style={{
-            height: "8vh",
-            display: "flex",
-            justifyContent: "left",
-            alignItems: "center",
-            paddingLeft: "10px",
-          }}
-        >
-          <div style={{ height: "60px", marginTop: "20px" }}>
-            <Logo />
-          </div>
+      <div className="top-left">
+        <div className="logo-container">
+          <Logo />
         </div>
+      </div>
+      <div className="top-right">
+        <button
+          onClick={() => setNavbarOpen(true)}
+          className="menu-button-wrapper"
+        >
+          <MenuButton />
+        </button>
+      </div>
+      <div className="left-side">
         <div className="home_side">
           <img
             src="/QuestionMark.png"
             alt="Küsimärk"
-            style={{ height: "25%", objectFit: "cover", filter: "invert(100)" }}
+            className="question-img"
           />
           <Link to="/daily-product">
             <button className="daily-product-button">Päevatoode</button>
           </Link>
         </div>
       </div>
-      <div style={{ backgroundColor: "white", flexGrow: 1 }}>
-        <div
-          style={{
-            height: "8vh",
-            display: "flex",
-            justifyContent: "right",
-            alignItems: "center",
-          }}
-        >
-          <button
-            onClick={() => setNavbarOpen(true)}
-            style={{
-              background: "none",
-              color: "inherit",
-              border: "none",
-              padding: "0",
-              font: "inherit",
-              cursor: "pointer",
-              outline: "inherit",
-              margin: "20px",
-            }}
-          >
-            <MenuButton />
-          </button>
-        </div>
+      <div className="right-side">
         <div className="home_side">
-          <div
-            style={{
-              width: "100%",
-              height: "25%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+          <div className="center-box">
             <img
               src="../../public/karp.png"
               alt="Saagikast"
-              style={{ height: "180%", objectFit: "cover", overflow: "hidden" }}
+              className="big-img"
             />
           </div>
           <Link to="/catalogue">
