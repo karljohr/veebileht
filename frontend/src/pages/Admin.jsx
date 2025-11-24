65d2e5e589fc129bda5c5fefc9c1fa8caec7113c
Karl-Johannes Ränkel <karljr@taltech.ee>
2025-11-24 21:41:42 +0200

    Lisasin admin lehele päevatoote kasti.

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -5,9 +5,11 @@ import ProfileButton from "../components/ProfileButton.jsx";
 function Admin() {
   const [prizes, setPrizes] = useState([]);
   const [prizePage, setPrizePage] = useState(1);
+  const [productPage, setProductPage] = useState(0);
   const [isFlipped, setIsFlipped] = useState(false);
   const [chosenBox, setChosenBox] = useState(1);
   const [chosenPrize, setChosenPrize] = useState("");
+  const [dayProducts, setDayProducts] = useState([]);
   const token = localStorage.getItem("token");
 
   const fetchPrizes = async () => {
@@ -20,17 +22,39 @@ function Admin() {
     }
   };
 
+  const fetchDayProducts = async () => {
+    await fetch("http://localhost:5000/api/dayproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setDayProducts(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     fetchPrizes();
+    fetchDayProducts();
   }, []);
 
   const getPrizesByBox = (box) => {
     return prizes.filter((prize) => prize.boxtype === box);
   };
 
-  const cardFlip = (event) => {
+  const cardFlip = (event, id) => {
     if (isFlipped) {
-      addBox(event);
+      switch (id) {
+        case 1:
+          addBox(event);
+          break;
+        case 2:
+        // Delete a daily product
+        case 3:
+        // Add a daily product
+        default:
+          break;
+      }
     } else {
       setIsFlipped(!isFlipped);
     }
@@ -40,16 +64,18 @@ function Admin() {
     event.preventDefault();
     // Add the submited box to the database
     try {
-      const response = await fetch("http://localhost:5000/api/add-box", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ boxtype: chosenBox, prize: chosenPrize }),
-      });
+      if (chosenPrize !== "") {
+        const response = await fetch("http://localhost:5000/api/add-box", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ boxtype: chosenBox, prize: chosenPrize }),
+        });
 
-      if (!response.ok) throw new Error("Failed to add prize");
+        if (!response.ok) console.log("Failed to add prize");
+      }
 
       await fetchPrizes();
 
@@ -74,13 +100,26 @@ function Admin() {
         body: JSON.stringify({ boxtype: prize.boxtype, prize: prize.prize }),
       });
 
-      if (!response.ok) throw new Error("Failed to delete");
+      if (!response.ok) console.log("Failed to delete");
 
       await fetchPrizes();
     } catch (error) {
       console.error("Error:", error);
     }
   };
+
+  function getCurrentPrice() {
+    if (dayProducts[productPage]) {
+      const time = new Date();
+      const step = Math.floor(
+        (dayProducts[productPage].startprice -
+          dayProducts[productPage].endprice) /
+          24,
+      );
+      return dayProducts[productPage].startprice - step * time.getHours();
+    }
+    return "error";
+  }
 
   const prizeTitles = {
     1: "Haruldane saagikast",
@@ -158,7 +197,7 @@ function Admin() {
             height="90%"
             text="Lisa auhind"
             fontSize="70%"
-            onClickOptions={cardFlip}
+            onClickOptions={(event) => cardFlip(event, 1)}
           />
           <button
             className="admin_button"
@@ -175,6 +214,57 @@ function Admin() {
       <div className="admin_box admin_content_box">
         <h4>Päevatoode</h4>
         <hr />
+        <div className="admin_box admin_dayproduct_box">
+          <img
+            src={`/${dayProducts[productPage]?.picture}.jpg`}
+            alt={"Picture"}
+            className="admin_box_image"
+          />
+          <p>{dayProducts[productPage]?.name}</p>
+          <p>{dayProducts[productPage]?.description}</p>
+          <p>
+            {dayProducts[productPage]?.startprice}€ > {getCurrentPrice()}€ >{" "}
+            {dayProducts[productPage]?.endprice}€
+          </p>
+        </div>
+        <div className="left_right_goodnight">
+          <button
+            className="admin_button"
+            onClick={() => {
+              if (productPage > 0) {
+                setProductPage(productPage - 1);
+              }
+            }}
+          >
+            {"<"}
+          </button>
+          <ProfileButton
+            width="6rem"
+            height="90%"
+            text="Kustuta"
+            fontSize="70%"
+            bcolor={"rgb(111,0,19)"}
+            color={"white"}
+            // onClickOptions={(event) => cardFlip(event, 2)}
+          />
+          <ProfileButton
+            width="6rem"
+            height="90%"
+            text="Lisa"
+            fontSize="70%"
+            // onClickOptions={(event) => cardFlip(event, 3)}
+          />
+          <button
+            className="admin_button"
+            onClick={() => {
+              if (productPage < dayProducts.length - 1) {
+                setProductPage(productPage + 1);
+              }
+            }}
+          >
+            {">"}
+          </button>
+        </div>
       </div>
     </div>
   );
